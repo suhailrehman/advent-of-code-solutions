ec9406d78aefe88bc7e02e21ee1c5646caa1d729
Suhail Rehman <suhailrehman@gmail.com>
2025-12-01 15:13:39 -0600

    '25 day01

---
+++ 2025/create_day.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Get the day number from the command line argument
+DAY_NUM=$1
+
+# Pad with leading zero if necessary
+DAY_PADDED=$(printf "%02d" "$DAY_NUM")
+
+echo "Creating directory for Day $DAY_PADDED..."
+
+# Create the directory
+mkdir -p "day$DAY_PADDED"
+
+# Create the input files
+touch "day$DAY_PADDED/sample.txt"
+touch "day$DAY_PADDED/full.txt"
+
+# Create the jupyter notebook
+touch "day$DAY_PADDED/code.ipynb"
